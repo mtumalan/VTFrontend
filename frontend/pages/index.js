0641b3b9784a4484203a720d63e9ed7fbcba58a3
Gabriel Cordova <gabrielcordovau@gmail.com>
2025-06-03 20:36:57 -0600

    more features
    add api fefching
    add dynamic cards for home model slider
    refactor unused components

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,33 +1,20 @@
 import Head from "next/head";
-import FaqHomeOne from "../components/common/faq/faq-home-one";
-import FeatureSectionTwo from "../components/common/feature/feature-section-two";
-import ClientSliderOne from "../components/common/sliders/client/client-slider-one";
-import TestimonialSliderOne from "../components/common/sliders/testimonial/testimonial-slider-one";
-import ContentWithPhotoSection from "../components/home/content-with-photo-section";
-import ContentWithPhotoSectionTwo from "../components/home/content-with-photo-section-two";
-import CtaHomeOne from "../components/home/cta-home-one";
+import CardSliderOne from "../components/common/sliders/card/card-slider-one";
 import HeroSection from "../components/home/hero-section";
-import VideoSectionTwo from "./../components/common/video/video-section-two";
+import Team from "../components/home/Team";
 
 export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>fugu - index -01</title>
+				<title>Index</title>
 			</Head>
 			<HeroSection />
-			<ClientSliderOne />
-			<FeatureSectionTwo />
-			<ContentWithPhotoSection />
-			<VideoSectionTwo />
-			<ContentWithPhotoSectionTwo />
-			<TestimonialSliderOne />
-			<FaqHomeOne />
-			<CtaHomeOne />
+			<CardSliderOne />
+			<Team />
 		</>
 	);
 }
-
 export async function getStaticProps() {
 	return { props: { header: "one", footer: "one" } };
 }
---
+++ frontend/utils/api.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = "http://localhost:8000/api";
+export const MODELS_ENDPOINT = `${API_BASE_URL}/models/`;
